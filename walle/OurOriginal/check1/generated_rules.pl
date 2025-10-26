da50f53ba80653b922549a08b24b18729082f357
chkt22058 <145964587+chkt22058@users.noreply.github.com>
2025-10-26 20:52:01 +0900

    Delete walle/OurOriginal directory

--- walle/OurOriginal/check1/generated_rules.pl
+++
@@ -1,8 +0,0 @@
-action_failed(put(Item, Receptacle)) :-
-    item_in_location(Item, ItemLoc),
-    item_in_location(Receptacle, ReceptacleLoc),
-    ItemLoc \= ReceptacleLoc.
-
-action_failed(put(Item, Receptacle)) :-
-    items_in_location(Receptacle, _),
-    location_status(Receptacle, full).
