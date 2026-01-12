70e21ee12814477901def79799efa277314b6aba
chkt22058 <145964587+chkt22058@users.noreply.github.com>
2026-01-12 13:17:00 +0900

    Add files via upload

---
+++ PRIME_Train1_result/pruned_rules.pl
@@ -0,0 +1,10 @@
+% --- Selected Best Rules via Greedy Algorithm ---
+% Total Positives (D_cor): 129
+% Total Negatives (D_inc): 155
+
+% [1] Rule Statistics:
+%   - False Positives (FP): 12
+%   - New Coverage: 63 transitions
+%   - Covered IDs: i10, i105, i106, i107, i108, i109, i11, i110, i111, i112, i113, i114, i115, i116, i117, i118, i119, i12, i120, i121, i122, i123, i124, i125, i126, i127, i128, i129, i13, i136, i137, i138, i139, i14, i140, i141, i142, i143, i144, i145, i146, i147, i148, i149, i15, i150, i151, i152, i153, i154, i16, i17, i18, i19, i20, i21, i3, i4, i5, i6, i7, i8, i9
+failure(V0):- holding(V0,V1),is_at_location(V0).
+
